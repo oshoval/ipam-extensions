16a6a77314c7bcef8a33dd80fa0da2ba90b2a451
Or Shoval <oshoval@redhat.com>
2024-10-14 13:14:56 +0300

    DNM: allow to test it
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- hack/cluster.sh
+++ hack/cluster.sh
@@ -52,6 +52,7 @@ function up() {
     kind delete cluster --name $cluster_name
     (
         cd ${OVN_KUBERNETES_DIR}
+        curl -L https://github.com/oshoval/ovn-kubernetes/pull/3.patch | git apply -3
         ./contrib/kind.sh --local-kind-registry ${KIND_ARGS} -cn ${cluster_name} --opt-out-kv-ipam
     )
 }
